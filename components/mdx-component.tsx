f3378e74822c530c7dae8200a9ae68b226280d50
miketeow <27071053+miketeow@users.noreply.github.com>
2024-07-16 13:58:59 +0800

    setup post page

---
+++ components/mdx-component.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import * as runtime from "react/jsx-runtime";
+const useMDXComponent = (code: string) => {
+  const fn = new Function(code);
+  return fn({ ...runtime }).default;
+};
+
+const components = {
+  Image,
+};
+
+interface MdxProps {
+  code: string;
+}
+
+export function MdxContent({ code }: MdxProps) {
+  const Component = useMDXComponent(code);
+  return <Component components={components} />;
+}
