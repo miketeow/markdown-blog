137be38aa2a9b1390d2c7eb1ebbb61ca54d7a25c
miketeow <27071053+miketeow@users.noreply.github.com>
2024-07-17 17:54:13 +0800

    remove open graph

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -24,33 +24,16 @@ export async function generateMetadata({
   if (!post) {
     return {};
   }
-  const ogSearchParams = new URLSearchParams();
-  ogSearchParams.set("title", post.title);
   return {
     title: post.title,
     description: post.description,
     authors: {
       name: siteConfig.author,
     },
-    openGraph: {
-      title: post.title,
-      description: post.description,
-      type: "article",
-      url: post.slug,
-      images: [
-        {
-          url: `/api/og?${ogSearchParams.toString()}`,
-          width: 1200,
-          height: 630,
-          alt: post.title,
-        },
-      ],
-    },
     twitter: {
       card: "summary_large_image",
       title: post.title,
       description: post.description,
-      images: [`/api/og?${ogSearchParams.toString()}`],
     },
   };
 }
